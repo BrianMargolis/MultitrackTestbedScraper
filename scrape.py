921df91c821c35f04db5dc11831814c85ca7b3e4
Brian Margolis <Brian.M.Margolis@gmail.com>
2018-08-12 22:43:03 -0500

    Add progress bars, ability to cache HTTP responses

--- scrape.py
+++ scrape.py
@@ -3,6 +3,7 @@ import urllib.request
 
 import bs4
 import requests
+from progress.bar import Bar
 
 
 def mock_http(url_id):
@@ -26,13 +27,14 @@ def mock_http(url_id):
 
     response = requests.request("GET", url, headers=headers, params=querystring)
 
-    response = json.loads(response.text)
+    response = json.loads(response.text)['0']
     return response
 
 
 def get_title_urls(n_pages):
     ids = []
     base_domain = 'http://multitrack.eecs.qmul.ac.uk/'
+    bar = Bar("Scraping for IDs...", max=n_pages)
     for page_num in range(1, n_pages + 1):
         sauce = urllib.request.urlopen(base_domain + '/?page={0}'.format(page_num)).read()
         soup = bs4.BeautifulSoup(sauce, 'lxml')
@@ -44,23 +46,36 @@ def get_title_urls(n_pages):
             if href and "query=title" in href:
                 ids.append(url.get('id'))
 
+        bar.next()
+
+    bar.finish()
     return ids
 
 
 def main():
-    json_path = 'url_ids.json'
-    regenerate_title_urls = False
+    ids_path = 'url_ids.json'
+    responses_path = 'responses.json'
+    cache_ids = True
+    cache_responses = False
 
-    if regenerate_title_urls:
-        with open(json_path, 'w') as outfile:
+    if cache_ids:
+        with open(ids_path) as json_file:
+            url_ids = json.load(json_file)
+    else:
+        with open(ids_path, 'w') as outfile:
             url_ids = get_title_urls(24)
             json.dump(url_ids, outfile)
-    else:
-        with open(json_path) as json_file:
-            url_ids = json.load(json_file)
 
-    for url_id in url_ids:
-        mock_http(url_id)
+    if cache_responses:
+        with open(responses_path) as f:
+            responses = json.load(f)
+    else:
+        responses = {}
+        bar = Bar("Mocking HTTP calls...", max=len(url_ids))
+        for url_id in url_ids:
+            responses[url_id] = mock_http(url_id)
+            bar.next()
+        bar.finish()
 
 
 if __name__ == "__main__":
